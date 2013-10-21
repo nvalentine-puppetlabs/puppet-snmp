9aa11e3f27d611cdcfd0670a94faf1e3fdbf47e4
Nathan Valentine <nathan@puppetlabs.com>
2013-10-21 16:42:44 -0700

    remove version param

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,6 @@ class snmp(
 
   validate_string($ro_community)
   if $rw_community { validate_string($rw_community) }
-  valdate_re($version, '^v[1-3]$')
 
   # PFS (sort of)
   class{'snmp::install':} -> class{'snmp::config':} ~> class{'snmp::service':}
